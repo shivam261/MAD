0df191f9d13c35aedb70b57bc82cdcab54242c62
shivam <shivam1705of@gmail.com>
2023-05-14 20:48:40 +0530

    first commit

---
+++ Child.java
@@ -0,0 +1,7 @@
+public class Child extends Mother {
+  public static void main(String[] args) {
+    short s = 9;
+    System.out.println(sum(s, 9));
+
+  }
+}
---
+++ Mother.java
@@ -0,0 +1,6 @@
+public class Mother {
+  int sum(int i, int j) {
+    return i + j;
+
+  }
+}
